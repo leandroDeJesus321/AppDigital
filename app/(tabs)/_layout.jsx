94f9f1ce7be249a9a919e5fde9b316d700ae7842
leandrodejesus <lean_silva100@hotmail.com>
2024-12-06 08:57:13 -0300

    Fui interropido enquanto codava... pqp

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -25,15 +25,51 @@ const TabsLayout = () => {
     <>
     <Tabs
     screenOptions={{
-      tabBarShowLabel: false 
+      tabBarShowLabel: false,
+      tabBarActiveTintColor: '#FFA001',
+      tabBArInactiveTinColor: '#CDCDE0',
+      tabBarStyle:{
+        background: '#161622',
+        borderTopWidth: 1,
+        borderTopColor: '#232533',
+        height: 84,
+      }
     }}
     >
+      {/* Tabela do botão de home page */}
       <Tabs.Screen 
         name='home' 
         options={{
           title: "Home", 
           headerShown: false, 
           tabBarIcon: ({color, focused}) =>(<TabIcon icon={icons.home} color={color} name="Home" focused={focused}/>)}} 
+          
+          />
+          {/* Tela de favoritos */}
+          <Tabs.Screen 
+        name='favoritos' 
+        options={{
+          title: "Favoritos", 
+          headerShown: false, 
+          tabBarIcon: ({color, focused}) =>(<TabIcon icon={icons.bookmark} color={color} name="Favorito" focused={focused}/>)}} 
+          
+          />
+          {/* Criação */}
+          <Tabs.Screen 
+        name='create' 
+        options={{
+          title: "Create", 
+          headerShown: false, 
+          tabBarIcon: ({color, focused}) =>(<TabIcon icon={icons.plus} color={color} name="Create" focused={focused}/>)}} 
+          
+          />
+          {/* Perfil do usuário */}
+          <Tabs.Screen 
+        name='perfil' 
+        options={{
+          title: "Perfil", 
+          headerShown: false, 
+          tabBarIcon: ({color, focused}) =>(<TabIcon icon={icons.profile} color={color} name="Perfil" focused={focused}/>)}} 
           
           />
     </Tabs>
